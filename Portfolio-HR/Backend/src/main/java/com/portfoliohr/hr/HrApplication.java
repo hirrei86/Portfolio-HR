85aa0d97ec526d32d786480fca023b2776a8ada2
hirrei86 <95895589+hirrei86@users.noreply.github.com>
2022-05-22 18:34:52 -0300

    Conexion de backend con frontend
    Se realizo la integracion de base de datos con el backend y luego se unieron al frontend

--- Portfolio-HR/Backend/src/main/java/com/portfoliohr/hr/HrApplication.java
+++ Portfolio-HR/Backend/src/main/java/com/portfoliohr/hr/HrApplication.java
@@ -1,4 +1,4 @@
-package com.portfolio.HR;
+package com.portfoliohr.hr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- Portfolio-HR/Backend/src/test/java/com/portfoliohr/hr/HrApplicationTests.java
+++ Portfolio-HR/Backend/src/test/java/com/portfoliohr/hr/HrApplicationTests.java
@@ -1,4 +1,4 @@
-package com.portfolio.HR;
+package com.portfoliohr.hr;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
